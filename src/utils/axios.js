ec38fb6b267cc4ca4a3296d97f60d47f40913c86
Rizal Haryaputra <rzlhryptr@gmail.com>
2026-04-11 06:58:21 +0700

    feat: add user form view with smart password handling for edit mode

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,33 +1,33 @@
-import axios from 'axios';
+    import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    }
-});
-
-// Interceptor Request: Otomatis menyisipkan Token JWT sebelum request dikirim
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-// Interceptor Response: Tangani error 401 (Token kedaluwarsa / Belum login)
-api.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response && error.response.status === 401) {
-            // Hapus token yang tidak valid dan arahkan paksa ke halaman login
-            localStorage.removeItem('token');
-            window.location.href = '/login';
+    const api = axios.create({
+        baseURL: 'http://127.0.0.1:8000/api',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
         }
-        return Promise.reject(error);
-    }
-);
+    });
 
-export default api;
+    // Interceptor Request: Otomatis menyisipkan Token JWT sebelum request dikirim
+    api.interceptors.request.use(config => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    });
+
+    // Interceptor Response: Tangani error 401 (Token kedaluwarsa / Belum login)
+    api.interceptors.response.use(
+        response => response,
+        error => {
+            if (error.response && error.response.status === 401) {
+                // Hapus token yang tidak valid dan arahkan paksa ke halaman login
+                localStorage.removeItem('token');
+                window.location.href = '/login';
+            }
+            return Promise.reject(error);
+        }
+    );
+
+    export default api;
